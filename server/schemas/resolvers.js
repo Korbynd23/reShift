3e7e77b68fa954f24bd42aa3888c3c7a8d9537ac
Nicholas Bales <nickb96@icloud.com>
2022-10-11 14:30:45 -0400

    catching up to main

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,17 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Profile } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find()
+    },
+
+    user: async (parent, {userId}) => {
+      return User.findOne({ _id: userId});
+    }
+  }
+}
+
+module.exports = resolvers
