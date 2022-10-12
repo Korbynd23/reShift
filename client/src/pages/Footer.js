844131d45960b63b927cb4b942a3647e4311ad50
meyerbw10 <109110828+meyerbw10@users.noreply.github.com>
2022-10-12 11:44:03 -0400

    adding Home, Footer.js and Home and Footer CSS

---
+++ client/src/pages/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer() {
+
+  return (
+    <footer className="footer">
+      <h1>Welcome</h1>
+    </footer>
+  );
+}
+
+export default Footer;
