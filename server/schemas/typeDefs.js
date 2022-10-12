bbfc7932053f39d728439792b98dd14c65311749
kdicari24@gmail.com <kdicari24@gmail.com>
2022-10-12 10:45:34 -0400

    was able to start server.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,13 +4,13 @@ const typeDefs = gql`
   type Employee {
     _id: ID
     name: String
-    password: INT
+    password: Int
   }
 
-  type Auth {
-    token: ID!
-    employee: Employee
-  }
+  # type Auth {
+  #   token: ID!
+  #   employee: Employee
+  # }
 
   type Query {
     employees: [Employee]!
@@ -18,12 +18,12 @@ const typeDefs = gql`
     me: Employee
   }
 
-  type Mutation {
-    addEmployee(name: String!, password:INT!, employeeId:INT!): Auth
-    login(name: String!, password: INT!): Auth
+  # type Mutation {
+  #   addEmployee(name: String!, password:INT!, employeeId:INT!): Auth
+  #   login(name: String!, password: INT!): Auth
 
-    removeEmployee: Employee
-  }
+  #   removeEmployee: Employee
+  # }
 `;
 
 module.exports = typeDefs;
