6138601f71bcd0a89c88e4fde7bbadfa2d846494
kdicari24@gmail.com <kdicari24@gmail.com>
2022-10-13 11:21:02 -0400

    added html for startShift/endShift

--- client/src/pages/endShift.js
+++ client/src/pages/endShift.js
@@ -2,11 +2,23 @@ import React from 'react';
 import '../styles/app.css';
 
 function endShift() {
-// End shift same style box as homeLogin page except with 5 face survey and "End Shift" button. 
-// upon clicking "end shift" will need to return to homelogin page and make sure values are cleared in forms
+  // End shift same style box as homeLogin page except with 5 face survey and "End Shift" button. 
+  // upon clicking "end shift" will need to return to homelogin page and make sure values are cleared in forms
   return (
     <div className="endBody">
-      <h1>You are on the clock out survey Page!</h1>
+      <div class="card w-50">
+        <div class="card-body text-center">
+          <h5 class="card-title">Hows your mood today?</h5>
+          <h1>
+            <button class="link">😫</button>
+            <button class="link">😔</button>
+            <button class="link">😐</button>
+            <button class="link">😊</button>
+            <button class="link">😁</button>
+          </h1>
+          <button type="button" class="btn btn-primary btn-lg btn-block">End Shift</button>
+        </div>
+      </div>
     </div>
   );
 }
